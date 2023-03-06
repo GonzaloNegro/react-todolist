564ddc6dde809fa43e049da50373f86fff7fe9ee
Gonzalo Negro <gonzalonnegro@gmail.com>
2023-03-06 20:11:25 -0300

    Complete and delete item

--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -2,26 +2,18 @@ import React from 'react';
 
 function TodoItem(props){
 
-    const onComplete = () =>{
-        alert("Ya completaste el todo " + props.text);
-    }
-
-    const onDelete = () =>{
-        alert("Borraste el todo " + props.text);
-    }
-
     return(
         <li className='todoItem'>
             <span 
                 className={`icon icon-check ${props.completed && 'icon-check--active'}`}
-                onClick={onComplete}
+                onClick={props.onComplete}
             >
             ✓
             </span>
             <p className={`todoItem-p ${props.completed && 'todoItem-p--complete'}`}>{props.text}</p>
             <span 
                 className='icon icon-delete'
-                onClick={onDelete}
+                onClick={props.onDelete}
             >
                 X
             </span>
